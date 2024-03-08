e3c8a0da23bbe3ab4d3ac81f8873c2904a37cbc4
akshar1277 <bhalani.akshar1277@gmail.com>
2024-03-08 11:28:03 +0530

    key changed

--- gfg/info.py
+++ gfg/info.py
@@ -2,5 +2,5 @@
 EMAIL_USE_TLS=True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = '1277keyur@gmail.com'
-EMAIL_HOST_PASSWORD = 'jrrbhilvmlcttsjp'
+EMAIL_HOST_PASSWORD = 'lekgqvjsyhracgje'
 EMAIL_PORT = 587
